c5edc5ab9e2336cdb74ced25c526878c6e40d77d
RKtheDragon <rkgangar07@gmail.com>
2019-09-18 16:48:41 +0530

    Java Prj on GIT added

---
+++ Hello.java
@@ -0,0 +1,30 @@
+public class Hello{
+	
+	public static void main(String[] args){
+		for (int i=0; i<=10; i++){
+			System.out.println("Hello World..." + i );
+		}
+	}
+}
+
+
+// Java-Jenkins-Git  
+// (Folder name in GIT directory as well Repo name in GitHub)
+// Save this file as Hello.java in "Java-Jenkins-Git" Directory 
+// Go to Command Line of and go to path where you saved this file
+// In CLI type c:> javac Hello.java 
+// In the folder you may see it created Hello.class file there 
+// Now in CLI run c:> java Hello 
+// This will print Hello World 10 times 
+// In the CLI type: git init to make it a GIT repository
+// git status
+// git add .
+// git commit -m "First commit to add Hello world to GIT"
+//
+// git remote add origin https://github.com/RKtheDragon/Java-Jenkins-Git.git
+
+
+
+
+
+
